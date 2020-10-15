42befff6885c93a59ae19e00784bd85d063a6f4c
anas-ecam2020 <71826535+anas-ecam2020@users.noreply.github.com>
2020-10-15 22:33:15 +0200

    Update reader.go

--- exercises/exo2/reader.go
+++ exercises/exo2/reader.go
@@ -10,12 +10,20 @@ type spaceEraser struct {
 	r io.Reader
 }
 
+func (sp spaceEraser) Read(p []byte) (int, error) {
+	n, err := sp.r.Read(p)
+	count := 0
+	for i := 0; i < n; i++ {
+		if p[i] != 32 {
+			p[count] = p[i]
+			count++
+		}
+	}
+	return count, err
+}
+
 func main() {
 	s := strings.NewReader("H e l l o w o r l d!")
 	r := spaceEraser{s}
 	io.Copy(os.Stdout, &r)
 }
-
-// Implement a type that satisfies the io.Reader interface and reads from another io.Reader,
-// modifying the stream by removing the spaces.
-// Expected output: "Helloworld!"
